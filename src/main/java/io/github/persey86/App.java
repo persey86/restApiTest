9202497e7a643d965dbd20c0a0d356d2b47c039d
persey86 <persey86@ukr.net>
2016-07-19 20:41:25 +0300

    initial commit

---
+++ src/main/java/io/github/persey86/App.java
@@ -0,0 +1,10 @@
+package io.github.persey86;
+
+/**
+ * Created by Anastasia on 19.07.2016.
+ */
+public class App {
+    public static void main(String... args){
+        System.out.println("test");
+    }
+}
